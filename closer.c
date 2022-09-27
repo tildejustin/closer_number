69936e98442c95a438d8a2c0b327958b55d852e6
Justin McCubbin <spacepepper3.14@gmail.com>
2022-09-27 17:05:47 -0400

    added functions and more checking

--- closer.c
+++ closer.c
@@ -5,20 +5,24 @@
 
 int booleanify (int);
 void firstwins (), secondwins ();
+void numchecks (int, int, int);
+void enter ();
 
 int main() {
     int first, second, picked, pir_num, range;
     bool pir;
-    // init the random number generator
+    // init the random number generator so the number is different each time heh
     srand(time(NULL));
 
     printf("Basic Number Comparer\n~justin, 23 Sep. 2022\n\n");
-    printf("What is your upper bound? ");
+    printf("What is your number limit? ");
     scanf("%d", &range);
-    // generate a random number and take the modulus of range, then add one to get a number between 1 and the picked range
+    // generate a random number and take the modulus of it and range
+    // then add one to get a number between 1 and the picked range inclusive
     // not secure, but who cares
     picked = (rand() % range) + 1;
     // printf("picked: %d\n", picked);
+    // terrible implementation
     printf("Price is Right rules? (0/1) ");
     scanf("%d", &pir_num);
     // turn pir into a bool, doesn't matter at all but it's practice with c functions
@@ -27,16 +31,25 @@ int main() {
     scanf("%d", &first);
     printf("What is the second number? ");
     scanf("%d", &second);
-    // only does "price is right" rules if given a 1, else doesn't use them
     // printf("First num: %d, Second num: %d\n", first, second);
-    // terminates program if any number is less than 1
-    if (first < 1 || second < 1) {
-        printf("One or both numbers are less than one, terminating program\n");
-        exit(0);
+    numchecks(first, second, range);
+
+    // normal logic
+    if (!pir) {
+        // if (!(first - picked) && !(second - picked)) {
+        //     printf("Both numbers are correct\n");
+        if (abs(first - picked) == abs(second - picked)) {
+            printf ("Both numbers are the same distance away\n");
+            printf("The picked number was %d\n", picked);
+        } else if (abs(first - picked) < abs(second - picked)) {
+            firstwins (picked);
+        } else if (abs(second - picked) < abs(first - picked)) {
+            secondwins (picked);
+        }
     }
-    // TODO check if any number is above the range
-    // runs this logic if using "price is right" rules
-    if (pir) {
+
+    // price is right logic
+    else if (pir) {
         // runs only if both numbers don't exceed the picked number
         // prob could be implemented better
         if (!(picked - first < 0 && picked - second < 0)) {
@@ -56,30 +69,9 @@ int main() {
             printf("Both numbers went over\n");
             printf("The picked number was %d\n", picked);
         }
-    // runs if "price is right" rules are not being used
-    } else if(!pir) {
-        // if (!(first - picked) && !(second - picked)) {
-        //     printf("Both numbers are correct\n");
-        if (abs(first - picked) == abs(second - picked)) {
-            printf ("Both numbers are the same distance away\n");
-            printf("The picked number was %d\n", picked);
-        } else if (abs(first - picked) < abs(second - picked)) {
-            firstwins (picked);
-        } else if (abs(second - picked) < abs(first - picked)) {
-            secondwins (picked);
-        }
-        printf("press enter to close\n");
-        // TODO implement enter to exit
-        // while (true) {
-        //     if (key.enter) {
-        //         exit(0)
-        //     }
-        // }
-        // temp while enter to close is not yet implemented
-        scanf(" ");
     }
-    //     if(((first - picked) <= 0) and ((second - picked) <= 0));
-    //         printf("Both numbers are less than or equal to the picked number \n");
+
+    enter();
     return 0;
 }
 
@@ -93,11 +85,37 @@ int booleanify (int input) {
 }
 
 void firstwins (int picked) {
-    printf("First wins!\n");
-    printf("The picked number was %d\n", picked);
+    printf ("First wins!\n");
+    printf ("The picked number was %d\n", picked);
 }
 
 void secondwins (int picked) {
     printf("Second wins!\n");
     printf("The picked number was %d\n", picked);
+}
+
+void numchecks (int first, int second, int range) {
+    // terminates program if any number is less than 1
+    if (first < 1 || second < 1) {
+        printf("One or both numbers are less than one, terminating program\n");
+        enter();
+    }
+    // terminates program if any number is larger than range
+    if (first > range || second > range) {
+        printf("One or both of the numbers is higher than your set range. Termination immenent.\n");
+        enter();
+    }
+}
+
+void enter (void) {
+    printf("Press enter to exit");
+    // TODO implement enter to exit
+    // while (true) {
+    //     if (key.enter) {
+    //         exit(0)
+    //     }
+    // }
+    // temp solution
+    scanf(" ");
+    exit(0);
 }
